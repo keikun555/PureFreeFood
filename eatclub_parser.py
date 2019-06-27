04196624151065b208eb612cf77aee6e9726c5f0
Hongji Li <hli@hli--macbookpro11.purestorage.com>
2019-06-27 16:25:47 -0700

    Filter the wrong emails
    Remove /r in the infos by strip()

--- eatclub_parser.py
+++ eatclub_parser.py
@@ -6,14 +6,17 @@ import re
 def grab_info(html_text):
     soup = BeautifulSoup(html_text, 'html.parser')
     infos = {}
+    if "www.eatclub.com" not in html_text:
+        return None
     for table in soup.find_all('table'):
-        if "Your order is ready" in str(table.text):
-            infos["address"] = re.findall("at\\s+(.*)", table.text)[0]
+        if "Your order is ready" in str(table):
+            infos["address"] = re.findall("at\\s+(.*)", table.text)[0].strip()
         elif "rack-loc" in str(table):
             infos["location"] = table.find('span').text.strip()
         elif table.find('strong'):
             infos["food"] = table.find('strong').text.strip()
-            infos["restaurant"] = re.findall("from\\s+(.*)", table.text)[0]
+            infos["restaurant"] = re.findall("from\\s+(.*)", table.text)[0].strip()
         else:
             pass
     return infos
+
